49f1a7e3065eea42976ffde7de4b432cea3509fa
Alex <alexanderchapman@outlook.com>
2018-04-13 23:44:46 +0100

    Added event listner to trigger make grid

--- designs.js
+++ designs.js
@@ -2,9 +2,21 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+document.addEventListener('DOMContentLoaded', function(){
+
+    const form = document.querySelector('#sizePicker');
+    form.addEventListener('submit' , function(event){
+        event.preventDefault(); 
+        makeGrid();
+    });
+
+
+});
+
+
+
+
 
 function makeGrid() {
-
-// Testing upstream!
-
+    //alert("width:" + width + " height:" + height);
 }
