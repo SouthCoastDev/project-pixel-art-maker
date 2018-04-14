2548a77c0c24adeadfb8375e80cfb3d547dd5b95
Alex <alexanderchapman@outlook.com>
2018-04-14 20:54:37 +0100

    Completed makeGrid function and cells change color

--- designs.js
+++ designs.js
@@ -1,31 +1,38 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
 document.addEventListener('DOMContentLoaded', function(){
-
     const form = document.querySelector('#sizePicker');
+    const canvas = document.getElementById("pixelCanvas");
+
     form.addEventListener('submit' , function(event){
-        event.preventDefault(); 
-        makeGrid(form);
+        event.preventDefault();
+        makeGrid(form , canvas);
     });
 
+    //change color of tds added to current color selected by color picker.
+    canvas.addEventListener('click', function(event){
+        const color = document.getElementById('colorPicker').value;
+        if(event.target.nodeName === 'TD'){
+            event.target.style.cssText='background-color:'+ color +';';
+        }
+    });
 
 });
 
-
-
-
-
-function makeGrid(form) {
-
+function makeGrid(form , canvas) { 
     //get form values
     const height = form[0].value;
     const width = form[1].value;
-    const color = document.getElementById('colorPicker').value;
-    
-    
 
+    //remove all table cells added. 
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
 
-    alert("width:" + width + " height:" + height + " color:" + color);
+    //create grid
+    for(var i = 0 ; i < height ; i ++){
+        let tableRow = document.createElement('tr');
+        for(var j = 0 ; j < width ; j ++){
+            tableRow.appendChild(document.createElement('td'));
+        }       
+        canvas.appendChild(tableRow);   
+    }
 }
